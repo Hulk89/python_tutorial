ca8bc3f00860766f02ea83aea15b0ac3cbedbaf3
hulk89 <snuboy89@gmail.com>
2020-07-25 15:38:52 +0900

    wxpython tut

---
+++ wxpython_tut/first_app/first_app.py
@@ -0,0 +1,5 @@
+import wx # Create a new app, don't redirect stdout/stderr to a window.
+app = wx.App(False) # A Frame is a top-level window.
+frame = wx.Frame(None, wx.ID_ANY, "Hello Python") # Show the frame.
+frame.Show(True)
+app.MainLoop()
