08da7039a17787b3afcccd9673b480eec5f25350
hulk89 <snuboy89@gmail.com>
2020-08-20 23:14:39 +0900

    update recursive code to support 1.0rc4

--- hydra_1.x_rc_test/recursive_instantiate/main.py
+++ hydra_1.x_rc_test/recursive_instantiate/main.py
@@ -4,20 +4,20 @@ from omegaconf import DictConfig, OmegaConf
 def instantiate(conf, *args, **kwargs):
     """recursive instantiate를 지원하는 함수.
         primitive type이 아닌 object들은 
-        args에 List로 positional argument를 넣어주어야한다.
+        __args에 List로 positional argument를 넣어주어야한다.
     """
-    if hasattr(conf, "params"):
-        if 'args' in conf.params:
-            args = list(args) + [instantiate(i) for i in conf.params.args]
+    if hasattr(conf, "_target_"):
+        if '__args' in conf:
+            args = list(args) + [instantiate(i) for i in conf.__args]
             primitive_conf = OmegaConf.to_container(conf, resolve=True)
-            del primitive_conf['params']['args']
+            del primitive_conf['__args']
             conf = DictConfig(primitive_conf)
     return hydra.utils.instantiate(conf, *args, **kwargs)
 
 
 @hydra.main(config_path='conf', config_name='config')
 def my_app(cfg : DictConfig) -> None:
-    print(cfg.pretty())
+    print(OmegaConf.to_yaml(cfg))
     obj = instantiate(cfg.parent_module)
     print(obj)
 
